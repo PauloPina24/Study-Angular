e89000170d59104ca262a56c471321dde235b711
Paulo <Paulodev24@gmail.com>
2024-09-29 14:12:42 -0100

    Como usar o NgOnInit e NgDestroy do Angular

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  //ngOnInit e uma funcao para dizer que um componente inicializou
+  ngOnInit(): void {
+    console.log('NavBar Inicializou!')
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, type OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnDestroy {
+  ngOnDestroy(): void {
+    console.log('Destruiu')
+  } 
 }
