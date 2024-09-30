f49576bb9292ac295507aca8bfd9f318010c0f2e
Paulo <Paulodev24@gmail.com>
2024-09-30 17:15:49 -0100

    Como usar o Ng-Content do AngularComo usar o Ng-Content do Angular

---
+++ src/app/_components/baseui/baseui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseui',
+  templateUrl: './baseui.component.html',
+  styleUrl: './baseui.component.css'
+})
+export class BaseuiComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { BaseuiComponent } from './_components/baseui/baseui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BaseuiComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
